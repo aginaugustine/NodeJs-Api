dfa89e602d643e625312bb37629367816789fbbc
aginaugustine <70435960+aginaugustine@users.noreply.github.com>
2022-03-25 14:44:52 +0000

    Update unit_test-spec.js

--- tests/unit_test-spec.js
+++ tests/unit_test-spec.js
@@ -1,4 +1,4 @@
-// use test spec file name as description to allow navigation from the test results view
+// use test spec file name as description to allow navigation from the test results view.
 describe("my first test", function () {
 
     it("First test", function(done) {
